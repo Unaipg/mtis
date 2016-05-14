1cbe02040779f3a98d3628f64e9976534a3024f2
Unai Pastor <unaipg@gmail.com>
2016-05-14 17:16:23 +0200

    Implementación de los servicios de transportes y actividades

--- BusinessServices/src/org/example/www/activitiesservice/ActivitiesServiceSkeleton.java
+++ BusinessServices/src/org/example/www/activitiesservice/ActivitiesServiceSkeleton.java
@@ -5,45 +5,79 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
  */
-    package org.example.www.activitiesservice;
+package org.example.www.activitiesservice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ *  ActivitiesServiceSkeleton java skeleton for the axisService
+ */
+public class ActivitiesServiceSkeleton{     
     /**
-     *  ActivitiesServiceSkeleton java skeleton for the axisService
+     * Auto generated method signature 
+     * @param bookActivity 
+     * @return bookActivityResponse 
      */
-    public class ActivitiesServiceSkeleton{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param bookActivity 
-             * @return bookActivityResponse 
-         */
-        
-                 public org.example.www.activitiesservice.BookActivityResponse bookActivity
-                  (
-                  org.example.www.activitiesservice.BookActivity bookActivity
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#bookActivity");
-        }
+	public org.example.www.activitiesservice.BookActivityResponse bookActivity(org.example.www.activitiesservice.BookActivity bookActivity){
+		BookActivityResponse response = new BookActivityResponse();
+		response.localOut = true;
+		try{
+			Connection connection = null;
+     		Statement statement = null;
+     		Class.forName("com.mysql.jdbc.Driver");
+     		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mtis", "root", "");
+     		statement = connection.createStatement();
+     		String query = "insert into 'mtis','activitybooking'('id','activity') values(NULL, '" + bookActivity.localIn +"') ) ";
+     		statement.executeQuery(query);
+		}catch(Exception e){
+			System.err.println(e.getMessage());
+			response.localOut = false;
+		}
+		return response;
+	}
      
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getActivities 
-             * @return getActivitiesResponse 
-         */
-        
-                 public org.example.www.activitiesservice.GetActivitiesResponse getActivities
-                  (
-                  org.example.www.activitiesservice.GetActivities getActivities
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getActivities");
-        }
-     
+    /**
+     * Auto generated method signature
+     * @param getActivities 
+     * @return getActivitiesResponse 
+     */
+	public org.example.www.activitiesservice.GetActivitiesResponse getActivities(org.example.www.activitiesservice.GetActivities getActivities){
+		ArrayList<Activity> readActs = new ArrayList<>();
+	   	 try{
+	    		Connection connection = null;
+	    		Statement statement = null;
+	    		Class.forName("com.mysql.jdbc.Driver");
+	    		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mtis", "root", "");
+	    		statement = connection.createStatement();
+	    		String query = "SELECT a1.* FROM activities a1 INNER JOIN  cities c1 on (c1.name like '%" + getActivities.localIn +"%')";
+	    		ResultSet resultSet = statement.executeQuery(query);
+	    		while(resultSet.next()){
+	    			Activity tmpAct = new Activity();
+	    			tmpAct.setId(resultSet.getInt("id"));
+	    			//tmpAct.setDate(resultSet.getDate("date"));
+	    			tmpAct.setDescription(resultSet.getString("description"));
+	    			tmpAct.setName(resultSet.getString("name"));
+	    			tmpAct.setPrice(resultSet.getFloat("price"));
+	    			tmpAct.setType(resultSet.getString("type"));
+	    			readActs.add(tmpAct);
+	    		}
+	    	}catch(Exception e){
+	    		System.out.println(e.getMessage());
+	    	}
+		
+	   	Activities acts = new Activities();
+	   	acts.localItem = new Activity[readActs.size()];
+	   	for(int i = 0 ; i < readActs.size(); i++)
+	       	acts.localItem[i] = readActs.get(i+1);
+		
+	   	GetActivitiesResponse response = new GetActivitiesResponse();
+		response.setOut(acts);
+		return response;
     }
+ 
+}
     
--- BusinessServices/src/org/example/www/carservice/CarServiceSkeleton.java
+++ BusinessServices/src/org/example/www/carservice/CarServiceSkeleton.java
@@ -5,45 +5,77 @@
  * This file was auto-generated from WSDL
  * by the Apache Axis2 version: 1.6.3  Built on : Jun 27, 2015 (11:17:49 BST)
  */
-    package org.example.www.carservice;
-    /**
-     *  CarServiceSkeleton java skeleton for the axisService
-     */
-    public class CarServiceSkeleton{
-        
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param rentCars 
-             * @return rentCarsResponse 
-         */
-        
-                 public org.example.www.carservice.RentCarsResponse rentCars
-                  (
-                  org.example.www.carservice.RentCars rentCars
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#rentCars");
-        }
-     
-         
-        /**
-         * Auto generated method signature
-         * 
-                                     * @param getCars 
-             * @return getCarsResponse 
-         */
-        
-                 public org.example.www.carservice.GetCarsResponse getCars
-                  (
-                  org.example.www.carservice.GetCars getCars
-                  )
-            {
-                //TODO : fill this with the necessary business logic
-                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#getCars");
-        }
-     
-    }
+package org.example.www.carservice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.*;
+
+/**
+ *  CarServiceSkeleton java skeleton for the axisService
+ */
+
+public class CarServiceSkeleton{
     
+    /**
+     * Auto generated method signature 
+     * @param rentCars 
+     * @return rentCarsResponse 
+     */
+     public org.example.www.carservice.RentCarsResponse rentCars(org.example.www.carservice.RentCars rentCars){
+    	 RentCarsResponse response = new RentCarsResponse();
+    	 response.setOut(true);
+    	 try{
+         	Connection connection = null;
+     		Statement statement = null;
+     		Class.forName("com.mysql.jdbc.Driver");
+     		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mtis", "root", "");
+     		statement = connection.createStatement();
+     		String query = "insert into 'mtis','carbooking'('id','startdate','enddate') values(NULL, '";
+     		query += rentCars.getIn().localId +"','"+ rentCars.getIn().localStartdate.toString() +"','"+ rentCars.getIn().localEnddate.toString()+"') ) ";
+     		statement.executeQuery(query);
+         }catch(Exception e){
+         	System.out.println(e.getMessage());
+         	response.setOut(false);
+         }
+         return response;
+      }
+ 
+     
+    /**
+     * Auto generated method signature
+     * @param getCars 
+     * @return getCarsResponse 
+     */
+     public org.example.www.carservice.GetCarsResponse getCars(org.example.www.carservice.GetCars getCars){
+    	 ArrayList<Car> readCars = new ArrayList<>();
+    	 try{
+     		Connection connection = null;
+     		Statement statement = null;
+     		Class.forName("com.mysql.jdbc.Driver");
+     		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mtis", "root", "");
+     		statement = connection.createStatement();
+     		String query = "SELECT c.* FROM cars c INNER JOIN  cities c1 on (c1.name like '%" + getCars.localIn +"%')";
+     		ResultSet resultSet = statement.executeQuery(query);
+     		while(resultSet.next()){
+     			Car tmpCar = new Car();
+     			tmpCar.setId(resultSet.getInt("id"));
+     			tmpCar.setModel(resultSet.getString("model"));
+     			tmpCar.setPrice(resultSet.getFloat("price"));
+     			readCars.add(tmpCar);
+     		}
+     	}catch(Exception e){
+     		System.out.println(e.getMessage());
+     	} 
+    	Cars cars = new Cars();
+    	cars.localItem = new Car[readCars.size()];
+        for(int i = 0 ; i < readCars.size(); i++)
+        	cars.localItem[i] = readCars.get(i+1);
+         
+    	GetCarsResponse response = new GetCarsResponse();
+    	response.setOut(cars);
+    	return response;
+    }
+}
